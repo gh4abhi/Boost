108ae796970db3bc3eed9ac64bfc8487987369d6
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2021-08-20 21:26:45 +0530

    Added audio in the game.

---
+++ Boost/Assets/Scripts/Rocket_Controller.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket_Controller : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space))      // We can thrust while rotating.                
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            print(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+            print(Vector3.forward);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+            print(-Vector3.forward);
+        }
+    }
+}
