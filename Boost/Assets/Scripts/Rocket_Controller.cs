8f9057e91a75bcdd29a7c2745c7253827f090e50
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2021-08-26 16:33:36 +0530

    Added some new features.

--- Boost/Assets/Scripts/Rocket_Controller.cs
+++ Boost/Assets/Scripts/Rocket_Controller.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket_Controller : MonoBehaviour
 {
+    // todo - We have to fix a lighting bug. 
     Rigidbody rigidBody;
     AudioSource audioSource;
     [SerializeField]
     float rcsThrust = 100f;
     [SerializeField]
     float mainThrust = 100f;
+
+    enum State { Alive, Dying, Transcending };
+
+    State state = State.Alive;
 
     // Start is called before the first frame update
     void Start()
@@ -22,30 +25,52 @@ public class Rocket_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+        {
+            Thrust();
+            Rotate();
+        }
     }
+
+    // todo fix sound bug.
 
     void OnCollisionEnter(Collision collision)
     {
+        if(state!=State.Alive)
+        {
+            return;
+        }
          switch(collision.gameObject.tag)
         {
             case "Safe":
                 print("Ok");
                 break;
             case "Obstacles":
+                state = State.Dying;
                 print("No Fuel");
+                Invoke("LoadSceneOne", 1f);
                 break;
             case "Fuel" :
                 print("Fuel Up");
                 break;
             case "Finish":
                 print("You Win");
+                state = State.Transcending;
+                Invoke("LoadNextScene",1f);
                 break;
             default:
                 print("Not Known");
                 break;
         }
+    }
+
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene(1); // todo for more than 1 level.
+    }
+    void LoadSceneOne()
+    {
+        SceneManager.LoadScene(0); // todo for more than 1 level.
     }
 
     void Thrust()
